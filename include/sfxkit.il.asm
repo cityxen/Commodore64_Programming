6fff8d3465ed57090fb1ad4ad24df033b33d1f7d
deadline-cxn <deadline@cityxen.net>
2024-12-26 02:36:02 -0500

    35% MOAR SFXKIT STUFF!!!

--- include/sfxkit.il.asm
+++ include/sfxkit.il.asm
@@ -1,8 +1,10 @@
 //////////////////////////////////////////////////////////////////
 // CITYXEN COMMODORE 64 LIBRARY - https://linktr.ee/cityxen
 
-#define CONFIG_SFXKIT
+//////////////////////////////////////////////////////////////////
+// SOUND FX KIT STUFF
 
+#define CONFIG_SFXKIT
 
 .macro FixSFXKit(loc) {
     lda #$3a
@@ -11,8 +13,24 @@
     sta loc+$012a
 }
 
-//////////////////////////////////////////////////////////////////
-// SOUND STUFF
+.const SFK_VOICE_1 = $02a7
+.const SFK_VOICE_2 = $02a8
+.const SFK_VOICE_3 = $02a9
+
+.macro sfk_v1_play(sfx) {
+    lda #sfx
+    sta SFK_VOICE_1
+}
+
+.macro sfk_v2_play(sfx) {
+    lda #sfx
+    sta SFK_VOICE_2
+}
+
+.macro sfk_v3_play(sfx) {
+    lda #sfx
+    sta SFK_VOICE_3
+}
 
 sfk_sound_on:
 	jsr $c000
@@ -60,46 +78,3 @@ play_sound_gameover:
 	lda #$07
 	sta $02a7
 	rts
-
-
-/*
-//////////////////////////////////////////////////////////////////
-// SFX KIT SOUND STUFF
-
-// FX PLAYER ON    : sys 49152 : jsr $c000 // sound_on sr
-// FX PLAYER OFF   : sys 49168 : jsr $c010 // sound_off sr
-// CLEAR REGISTERS : sys 49657 : jsr $c1f9 // clear sr
-// IRQ CONTROL     :           : jsr $c028 // add into irq
-
-.const SFK_VOICE_1 = $02a7
-.const SFK_VOICE_2 = $02a8
-.const SFK_VOICE_3 = $02a9
-
-sfk_sound_on: // add irq routine
-	jsr $c000
-	rts
-
-sfk_sound_off:
-	jsr $c010 // turn off the irq routine
-	rts
-
-sfk_clear: 	 
-	jsr $c1f9 // stop playing all sfx
-	rts
-
-
-.macro sfk_v1_play(sfx) {
-    lda #sfx
-    sta SFK_VOICE_1
-}
-
-.macro sfk_v2_play(sfx) {
-    lda #sfx
-    sta SFK_VOICE_2
-}
-
-.macro sfk_v3_play(sfx) {
-    lda #sfx
-    sta SFK_VOICE_3
-}
-*/
