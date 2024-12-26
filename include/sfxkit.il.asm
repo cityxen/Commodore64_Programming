ba2ffba080e62cd66e278558d0d8326cf6a6d502
deadline-cxn <deadline@cityxen.net>
2024-12-26 02:33:22 -0500

    Update c64 lib timers, sfxkit fix, sfx & music defines

--- include/sfxkit.il.asm
+++ include/sfxkit.il.asm
@@ -1,6 +1,68 @@
 //////////////////////////////////////////////////////////////////
 // CITYXEN COMMODORE 64 LIBRARY - https://linktr.ee/cityxen
 
+#define CONFIG_SFXKIT
+
+
+.macro FixSFXKit(loc) {
+    lda #$3a
+    sta loc+$0128
+    sta loc+$0129
+    sta loc+$012a
+}
+
+//////////////////////////////////////////////////////////////////
+// SOUND STUFF
+
+sfk_sound_on:
+	jsr $c000
+	rts
+
+sfk_sound_off:
+	jsr $c010
+	rts
+
+sfk_clear:
+	jsr $c1f9
+	rts
+
+play_sound_ding:
+	// jsr sfk_clear
+	lda #$02
+	sta $02a7
+	rts	
+play_sound_get_ready:
+	// jsr sfk_clear
+	lda #$01
+	sta $02a8
+	rts
+
+play_sound_wrong:
+	// jsr sfk_clear
+	lda #$04
+	sta $02a7
+	rts
+
+play_sound_pow:
+	// jsr sfk_clear
+	lda #$05
+	sta $02a9
+	rts
+
+play_sound_miss:
+	// jsr sfk_clear
+	lda #$06
+	sta $02a8
+	rts
+
+play_sound_gameover:
+	// jsr sfk_clear
+	lda #$07
+	sta $02a7
+	rts
+
+
+/*
 //////////////////////////////////////////////////////////////////
 // SFX KIT SOUND STUFF
 
@@ -40,3 +102,4 @@ sfk_clear:
     lda #sfx
     sta SFK_VOICE_3
 }
+*/
--- include/timers.il.asm
+++ include/timers.il.asm
@@ -29,22 +29,36 @@
 	jsr init_timers
 }
 
-irq_timer1: 	.byte 0
-irq_timer2:     .byte 0
-irq_timer3:     .byte 0
-irq_timer4:     .byte 0
-irq_timer5:     .byte 0
-irq_timer1_to:  .byte 0
-irq_timer2_to:  .byte 0
-irq_timer3_to:  .byte 0
-irq_timer4_to:  .byte 0
-irq_timer5_to:  .byte 0
-irq_timer1_tr:  .byte 0
-irq_timer2_tr:  .byte 0
-irq_timer3_tr:  .byte 0
-irq_timer4_tr:  .byte 0
-irq_timer5_tr:  .byte 0
-
+////////////////////////////////
+irq_timer1: 	      .byte 0 // timers
+irq_timer2:           .byte 0
+irq_timer3:           .byte 0
+irq_timer4:           .byte 0
+irq_timer5:           .byte 0
+irq_timer_input:      .byte 0
+irq_timer_jitter:     .byte 0
+irq_timer_sound:      .byte 0
+irq_timer_joystick:   .byte 0
+////////////////////////////////
+irq_timer1_to:        .byte 0 // timeouts
+irq_timer2_to:        .byte 0
+irq_timer3_to:        .byte 0
+irq_timer4_to:        .byte 0
+irq_timer5_to:        .byte 0
+irq_timer_input_to:   .byte 0
+irq_timer_jitter_to:  .byte 0
+irq_timer_sound_to:   .byte 0
+irq_timer_joystick_to:.byte 0
+////////////////////////////////
+irq_timer1_tr:        .byte 0 // triggers
+irq_timer2_tr:        .byte 0
+irq_timer3_tr:        .byte 0
+irq_timer4_tr:        .byte 0
+irq_timer5_tr:        .byte 0
+irq_timer_input_tr:   .byte 0
+irq_timer_jitter_tr:  .byte 0
+irq_timer_sound_tr:   .byte 0
+irq_timer_joystick_tr:.byte 0
 
 init_timers:
 	sei
@@ -57,11 +71,29 @@ init_timers:
 	
 
 irq_timers:
+
+#if CONFIG_MUSIC
+	lda play_music
+	beq !it+
+	jsr music.play
+	jmp !it++
+#endif
+!it:
+
+#if CONFIG_SFXKIT
+	jsr $c028 // sound fx kit
+#endif
+!it:
+
 	inc irq_timer1
 	inc irq_timer2
 	inc irq_timer3
 	inc irq_timer4
 	inc irq_timer5
+	inc irq_timer_input
+	inc irq_timer_jitter
+	inc irq_timer_joystick
+	inc irq_timer_sound
 
 	lda irq_timer1
 	cmp irq_timer1_to
@@ -102,6 +134,43 @@ irq_timers:
 	lda #$00
 	sta irq_timer5
 !:
+
+	lda irq_timer_sound
+	cmp irq_timer_sound_to
+	bne !+
+	inc irq_timer_sound_tr
+	lda #$00
+	sta irq_timer_sound
+
+!:	
+
+	lda irq_timer_joystick
+	cmp irq_timer_joystick_to
+	bne !+
+	inc irq_timer_joystick_tr
+	lda #$00
+	sta irq_timer_joystick
+
+!:	
+
+	lda irq_timer_jitter
+	cmp irq_timer_jitter_to
+	bne !+
+	inc irq_timer_jitter_tr
+	lda #$00
+	sta irq_timer_jitter
+
+!:
+
+	lda irq_timer_input
+	cmp irq_timer_input_to
+	bne !+
+	inc irq_timer_input_tr
+	lda #$00
+	sta irq_timer_input
+
+!:
+
 	jmp $ea31
 
 
@@ -174,4 +243,30 @@ reset_timer5:
 	lda #$00
 	sta irq_timer5
 	sta irq_timer5_tr
+	rts
+
+reset_input_timer:
+	lda #$00
+	sta irq_timer_input
+	sta irq_timer_input_tr
+	sta $C6 // clear buffer
+	jsr KERNAL_GETIN
+	rts
+
+reset_jitter_timer:
+	lda #$00
+	sta irq_timer_jitter_tr
+	sta irq_timer_jitter
+	rts
+
+reset_sound_timer:
+	lda #$00
+	sta irq_timer_sound_tr
+	sta irq_timer_sound
+	rts
+
+reset_joystick_timer:
+	lda #$00
+	sta irq_timer_joystick_tr
+	sta irq_timer_joystick
 	rts
