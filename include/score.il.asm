2b791765a3d7ff63a5b9051d78da68c66c7ca29c
deadline-cxn <deadline@cityxen.net>
2025-01-02 14:36:50 -0500

    add is_score_zero subroutine

--- include/score.il.asm
+++ include/score.il.asm
@@ -11,7 +11,7 @@ score: // Bytes of the score in BCD
 // LSB --------------- MSB
 .byte $00,$00,$00,$00,$00
 
-score_str: .text "0000000099" // String of the score
+score_str: .text "0000000000" // String of the score
            .byte 0 
 score_digits:   .byte $09  // num of digits to display (default is 10)
 score_math_o:   .byte $00  // Math Val
@@ -164,6 +164,42 @@ score_sub:
 
     cld
 
+    bcs !+ // can't be less than 0
+    jsr score_reset
+!:
+
     jsr score_to_str
 
     rts
+
+
+is_score_zero:
+    lda #$00
+    sta score_zero
+    sed
+    clc
+    lda score
+    beq !+
+    inc score_zero
+!:
+    lda score+1
+    beq !+
+    inc score_zero
+!:
+    lda score+2
+    beq !+
+    inc score_zero
+!:
+    lda score+3
+    beq !+
+    inc score_zero
+!:
+    lda score+4
+    beq !+
+    inc score_zero
+!:
+    cld
+    lda score_zero
+    rts
+score_zero:
+.byte 0
